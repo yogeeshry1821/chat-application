8c35ab1012b471dba4dabe018677a5799708e1e7
Yogeesh <yryadappa100@gmail.com>
2024-12-05 03:08:34 +0530

    layout created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='bg-black h-screen text-white'>
+      <div className='h-[90vh]'>
+        </div>
+        <div className='flex w-full bg-white p-4 space-x-2'>
+          <input type="text" className='w-full flex-1 p-5 bg-gray-100 text-black border-2 rounded-md border-black ' />
+        <button className='bg-white p-5 border-2 rounded-md border-black text-black'> Send</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
